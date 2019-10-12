e22bfbef329d6df84f10828f7a1172b2c026273d
tonyz <zuccaro@gmail.com>
2019-10-12 15:46:57 -0400

    stubbed out cli project

---
+++ src/hex/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ZTC.Hexaminer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var argstr = string.Join(" ",args);
+            Console.WriteLine($"hex {argstr}");
+        }
+    }
+}
